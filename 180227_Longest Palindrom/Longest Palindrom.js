715068f2a2663a491ceb13e3c86e87875b35fab5
JinwooPark94 <wlsdntus4@gmail.com>
2018-02-28 00:05:37 +0900

    180228 Algorism Longest Palindrom

--- 180227_Longest Palindrom/Longest Palindrom.js
+++ 180227_Longest Palindrom/Longest Palindrom.js
@@ -1,18 +1,24 @@
+/*  가장 긴 팰린드롬
+    앞뒤를 뒤집어도 똑같은 문자열을 palindrome이라고 합니다.
+    longest_palindrom함수는 문자열 s를 매개변수로 입력받습니다.
+    s의 부분문자열중 가장 긴 palindrom의 길이를 리턴하는 함수를 완성하세요.
+    예를들어 s가 토마토맛토마토이면 7을 리턴하고 토마토맛있어이면 3을 리턴합니다.  */
+
 function longest_palindrom(s){
-  var result = 0;
-  var palindVal = s.split('');
-  var preV = '';
-  var data = '';
+  let result = 0;
+  let preV = '';
+  let data = '';
+  const palindVal = s.split('');
 
   // 함수를 완성하세요
-  for( var j = 0; j < s.length; j++ ){
+  for ( let i = 0; i < s.length; i++ ){
     data = '';
-    preV = palindVal[j];
+    preV = palindVal[i];
 
-    for( var i = j; i < s.length; i++ ){
-      data += palindVal[i];
+    for ( let j = i; j < s.length; j++ ){
+      data += palindVal[j];
 
-      if (preV === palindVal[i]) {
+      if (preV === palindVal[j]) {
 
         if (data === data.split('').reverse().join('')) {
           (result > data.length) ? result : result = data.length;
